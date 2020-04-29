7042335bfeafaa7ba079062a050d89b5b023ebf0
shuejiegong205@126.com <gongshujie@anong.com>
2020-04-29 11:07:53 +0800

    add digits

---
+++ php/AddDigits.php
@@ -0,0 +1,16 @@
+<?php
+class AddDigits
+{
+    public function addDigits($num)
+    {
+        $len = strlen($num);
+        if ($len == 1) {
+            return $num;
+        }   
+        $arr = str_split($num);
+        return $this->addDigits(array_sum($arr));
+    }
+}
+$obj = new Solution;
+$res = $obj->addDigits(38);
+var_dump($res);
